0108d3c6be966723500f057c0109f5141d2a90ea
brianryu <s.brianryu@gmail.com>
2019-02-21 15:56:31 -0500

    Added a Transfer class to transfer.rb & Added a BankAccount class to bank_account.rb

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,40 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    @sender.valid? && @receiver.valid?
+  end
+
+  def execute_transaction
+    initial_balance = 1000
+    if @sender.balance != initial_balance
+      self.status
+    else
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      self.status = "complete"
+    end
+
+    if self.valid? == false
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      self.status = "reversed"
+    end
+  end
+
 end
